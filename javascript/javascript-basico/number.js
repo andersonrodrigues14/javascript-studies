7b2ba4d9017e97d3b29ea9ed3c9a14b2d6baff60
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-08-22 15:46:39 -0300

    Iniciado módulo de JavaScript básico

---
+++ javascript/javascript-basico/number.js
@@ -0,0 +1,26 @@
+let idade = 58;
+
+let precoDolar= 6.20;
+
+console.log(typeof idade);
+console.log(typeof precoDolar);
+
+let soma = 3 + 5;
+let subtracao = 10 - 3;
+let multiplicacao = 10 * 4;
+let divisao = 9 / 7;
+
+console.log(`Soma: ${soma}`);
+console.log(`Subtração: ${subtracao}`);
+console.log(`Multiplicação: ${multiplicacao}`);
+console.log(`Divisão: ${divisao}`);
+
+let contador = 0;
+contador++;
+console.log(`Contador: ${contador}`);
+contador--;
+console.log(`Contador: ${contador}`);
+
+let saldo = 100;
+saldo += 50;
+console.log(`Saldo: ${saldo}`);
---
+++ javascript/javascript-basico/var-let-const.js
@@ -0,0 +1,8 @@
+// usando var fica fora do scopo {}
+var nome = "Anderson";
+
+// criada apenas dentro do spopo {}
+let sobrenome = "Rodrigues";
+
+//não pode mudar
+const cidade = "Caxia do Sul";
